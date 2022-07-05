3356eaf91d8f3eaa5ce187cccc907ee82527f565
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-04 23:57:55 -0500

    Update 1.php
    Add js component

--- phpjs/1.php
+++ phpjs/1.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
 </head>
 <body>
+    <h1>php</h1>
     <?php
         echo "Hello world";
         echo 10 + 10;
     ?>
+    <h1>JavaScript</h1>
+    <script>
+        document.write("Hello world");
+        document.write(10+10);
+    </script>
 </body>
 </html>
