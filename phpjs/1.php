012124eb30067b02d81b1630a2df8154a3b3277b
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-05 00:01:13 -0500

    Update 1.php
    Add results of php and js to compare

--- phpjs/1.php
+++ phpjs/1.php
@@ -9,10 +9,13 @@
         echo "Hello world";
         echo 10 + 10;
     ?>
+    // result : Hello world, 20 <- server side 
     <h1>JavaScript</h1>
     <script>
         document.write("Hello world");
         document.write(10+10);
     </script>
+    // result : document.write("Hello world"); <- web browser side
+                document.write(10+10);
 </body>
 </html>
