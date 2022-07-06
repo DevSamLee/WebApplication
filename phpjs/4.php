6c97c69bacfc59349062ae38a3a33548e48c0e49
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-05 22:20:37 -0500

    Add #2, 3, 4.php
    Add #2, 3, 4.php

---
+++ phpjs/4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>JavaScript</h1>
+	<script>
+		document.write(1 == 1); // true
+		document.write(2 == 1); // false
+	</script>
+	<h1>php</h1>
+	<?php
+		var_dump(1 == 1); // true
+		var_dump(2 == 1); // false		
+	?>
+</body>
+</html>
