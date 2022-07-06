5b55107904c0c73ed44764936b59c8f17a13fb27
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-05 22:19:48 -0500

    Add files via upload
    Add 2, 3, 4.php

---
+++ 2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>JavaScript</h1>
+	<script>
+		document.write(10+10); // 20
+		document.write("10"+"10"); // 1010
+	</script>
+	<h1>php</h1>
+	<?php
+		echo 10+10; // 20
+		echo "10"+"10"; // 20
+		echo "10"."10";	// 1010
+	?>
+</body>
+</html>
