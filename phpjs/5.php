2ac2256883962defab72f71c0af66bcaa5f09433
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-07 22:57:11 -0500

    Add files via upload
    Add 5.php and 6.php

---
+++ phpjs/5.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>JavaScript</h1>
+	<script>
+		result = (1 == 1);
+		if(result) {		// true
+			document.write("true");
+		} else {
+			document.write("false");
+		}
+	</script>
+	<h1>php</h1>
+	<?php
+		$result = (2 == 1);
+		if($result) {		// false
+			echo "true";
+		} else {
+			echo "false";
+		}	
+	?>
+</body>
+</html>
