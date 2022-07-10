0dd192321e48c8490061ecbca701572b903ef19d
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-09 23:49:50 -0500

    Update 8.php
    Add #8.php

--- phpjs/8.php
+++ phpjs/8.php
@@ -1,1 +1,25 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+  <h2>JavaScript</h2>
+	<script>
+		i = 0;
+    while(i < 10) {
+      document.write("Hello World<ba />");
+      i = i + 1;
+    }
+	</script>
+  
+  <h2>php</h2>
+  <?php
+    $i = 0;
+    while($i < 10) {
+      echo "Hello World<br />";
+      $i = $i + 1;
+    }
+  ?>
+</body>
+</html>
