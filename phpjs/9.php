9a7ebaddfe29b950372b2de08644c5418482df0a
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-11 22:56:27 -0500

    Create 9.php
    Add #9.php

---
+++ phpjs/9.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+  <h2>JavaScript</h2>
+	<script>
+		list = new Array("one", "two", "three");
+    document.write(list[0]);
+    document.write(list[1]);
+    document.write(list[2]);
+	</script>
+  
+  <h2>php</h2>
+  <?php
+    $list = array("one", "two", "three");
+    echo $list[0];
+    echo $list[1];
+    echo $list[2];
+  ?>
+</body>
+</html>
