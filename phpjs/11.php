a4675ef4d64232c715e4eab1666dcc00b303f5e3
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-14 23:50:08 -0500

    Create 11.php
    Add #11.php

---
+++ phpjs/11.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+  <h2>JavaScript</h2>
+	<script>
+		function a(){
+    document.write("Hello JS Function");
+    }
+    a();
+	</script>
+  
+  <h2>php</h2>
+  <?php
+    function a(){
+    echo "Hello PHP Function";
+    }
+    a();
+  ?>
+</body>
+</html>
