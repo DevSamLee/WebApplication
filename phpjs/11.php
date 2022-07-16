9c035d85fbbbeecf382d118b7d5e32bf1359d535
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-15 22:28:00 -0500

    Update 11.php
    Update #11.php

--- phpjs/11.php
+++ phpjs/11.php
@@ -5,19 +5,20 @@
 </head>
 <body>
   <h2>JavaScript</h2>
-	<script>
-		function a(){
-    document.write("Hello JS Function");
-    }
-    a();
-	</script>
+<script>
+	function a(input){
+	    return input + 1;
+	}
+	document.write(a(6));
+	prompt(a(6));
+</script>
   
-  <h2>php</h2>
-  <?php
-    function a(){
-    echo "Hello PHP Function";
-    }
-    a();
-  ?>
+<h2>php</h2>
+<?php
+	function a(){
+	echo "Hello PHP Function";
+	}
+	a();
+?>
 </body>
 </html>
