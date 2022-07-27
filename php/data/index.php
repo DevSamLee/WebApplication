1d870e64651b49d7eec5f9169e3c42a06de0eac5
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-26 23:02:38 -0500

    Update index.php

--- php/data/index.php
+++ php/data/index.php
@@ -1,18 +1,25 @@
+<?php
+    $conn = mysquli_connect('localhost','serveruser', 'gorgonzola7!');
+    mysqli_select_db($conn, 'serverside');
+    $result = mysqli_query($conn, 'SELECT * FROM javascript');   
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="http://localhost/style.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost:31337/webapplication/php/data/style.css">
     <title>JavaScript</title>
 </head>
 <body id="target">
     <header>
-        <h1><a href="http://localhost/php/index.php">JavaScript</a></h1>
+        <h1><a href="http://localhost:31337/webapplication/php/data/index.php">JavaScript</a></h1>
     </header>
     <nav>
         <ol>
            <?php
-                echo file_get_contents('list.txt');
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo '<li><a href="http://localhost:31337/webapplication/php/data/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+                }
             ?>
         </ol>
     </nav>
@@ -23,10 +30,10 @@
     <article>
         <?php
             if(!empty($_GET['id'])){
-                echo file_get_contents($_GET['id'].' .txt');
+                echo file_get_contents($_GET['id'].".txt");
             }
         ?>
     </article>
-    <script src="http://localhost/script.js"></script>
+    <script src="http://localhost:31337/webapplication/php/data/script.js"></script>
 </body>
 </html>
