6615fb4dbb3029322aae6efff3f1c17d6173b6e9
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-07-31 22:12:32 -0500

    Create 5.php
    Add htmlspecialchars() to 5.php

---
+++ php/data/5.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo htmlspecialchars('<a href="https://github.com/DevSamLee/">DevSamLee</a>');
+    ?>
+  </body>
+</html>
