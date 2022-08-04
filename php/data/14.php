646c1520de4333a275c5536ada362bdc20227ead
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-08-03 23:32:17 -0500

    Update 14.php
    Add mysqli_real_escape_string() for data sanitization

--- php/data/14.php
+++ php/data/14.php
@@ -1,7 +1,10 @@
 <?php
     $conn = mysquli_connect('localhost','serveruser', 'gorgonzola7!');
-    mysqli_select_db($conn, 'serverside');
-    $spl = "SELECT * FROM user WHERE name='".$_GET['name']."' AND password='".$_GET['password']."'";
+    mysqli_select_db($conn, 'serverside');    
+    $name = mysqli_real_escape_string($conn, $_GET['name']); // ' cannot be special char
+    $password = mysqli_real_escape_string($conn, $_GET['password']); // ' cannot be special char
+    $spl = "SELECT * FROM user WHERE name='".$name."' AND password='".$password."'";
+    echo $sql;
     $result = mysqli_query($conn, $sql);
     var_dump($result->num_rows);
 ?>
@@ -13,10 +16,10 @@
 </head>
 <body>
 	<? php
-		if($result->num_rows == "1") {
-			echo "Welcome.";
-		} else {
+		if($result->num_rows == "0") {
 			echo "Who are you?";
+		} else {
+			echo "Welcome.";
 		}
 	?>
 </body>
