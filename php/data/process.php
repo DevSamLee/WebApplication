35c370241a20e92c406920938c687e8042bdee65
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-08-04 21:50:18 -0500

    Update process.php

--- php/data/process.php
+++ php/data/process.php
@@ -1,5 +1,6 @@
 <?php
   require("lib/db.php");
+  db_init();
   $sql ="INSERT INTO topic (title, description, author, created) VALUES ('".$_POST['title']."', '".$_POST['description']."', '".$_POST['author']."', now())";
   $result = mysqli_query($conn, $sql);
   header('Location: http://localhost:31337/webapplication/php/data/index.php');
