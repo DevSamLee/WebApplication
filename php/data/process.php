3402f00d6d0536b42b635c786c6a8dc7cdbecbd9
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-08-05 22:58:55 -0500

    Update process.php
    Add parameters to reuse db_init

--- php/data/process.php
+++ php/data/process.php
@@ -1,6 +1,6 @@
 <?php
   require("lib/db.php");
-  db_init();
+  $conn = db_init("localhost", "root", "111111", "opentutorials");
   $sql ="INSERT INTO topic (title, description, author, created) VALUES ('".$_POST['title']."', '".$_POST['description']."', '".$_POST['author']."', now())";
   $result = mysqli_query($conn, $sql);
   header('Location: http://localhost:31337/webapplication/php/data/index.php');
