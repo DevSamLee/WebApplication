ba039d8d8a2514f4c471cd8bb9f28c0ea6c3b084
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-08-05 22:54:56 -0500

    Update db.php
    Remove static value in order to reuse it

--- lib/db.php
+++ lib/db.php
@@ -1,7 +1,7 @@
 <?php
-function db_init(){
-  $conn = mysquli_connect('localhost','serveruser', 'gorgonzola7!');
-  mysqli_select_db($conn, 'serverside');
+function db_init($host, $dbuser, $dbpw, $dbname){
+  $conn = mysquli_connect($host, $dbuser, $dbpw);
+  mysqli_select_db($conn, $dbname);
   return $conn;
 }
 ?>
