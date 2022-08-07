ebf7db1396e69c5d7ecc5e51896fec2d739eb214
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-08-06 23:59:38 -0500

    Update process.php

--- php/data/process.php
+++ php/data/process.php
@@ -1,6 +1,7 @@
 <?php
+  require("config/config.php");
   require("lib/db.php");
-  $conn = db_init("localhost", "root", "111111", "opentutorials");
+  $conn = db_init($config["host"], $config["dbuser"], $config["dbpw"], $config["dbname"]);
   $sql ="INSERT INTO topic (title, description, author, created) VALUES ('".$_POST['title']."', '".$_POST['description']."', '".$_POST['author']."', now())";
   $result = mysqli_query($conn, $sql);
   header('Location: http://localhost:31337/webapplication/php/data/index.php');
