2a626213603e65c9cf15f456e9acd79f7cd65693
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-08-08 23:56:40 -0500

    Update index.php

--- php/data/index.php
+++ php/data/index.php
@@ -22,32 +22,35 @@
     <header>
         <h1><a href="http://localhost:31337/webapplication/php/data/index.php">JavaScript</a></h1>
     </header>
-    <nav>
-        <ol>
-           <?php
-                while($row = mysqli_fetch_assoc($result)) {
-                    echo '<li><a href="http://localhost:31337/webapplication/php/data/index.php?id='.htmlspecialchars($row['id']).'">'.$row['title'].'</a></li>'."\n";
-                }
-            ?>
-        </ol>
-    </nav>
-    <div id="control">
-        <input type="button" value="white" id="white_btn" />
-        <input type="button" value="black" id="black_btn" />
-        <a href="http://localhost:31337/webapplication/php/data/write.php">Write</a>
-    </div>
-    <article>
-        <?php
-            if(!empty($_GET['id'])){
-                $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
-                $result = myquli_query($conn, $sql);
-                $row = mysqli_fetch_assoc($result);
-                echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
-                echo '<p>'.htmlspecialchars($row['name']).'</p>;
-                echo strip_tags($row['description'], '<a><h1><h2><h3><ul><li><ol>');
-            }
-        ?>
-    </article>
+    <div class="row">
+        <nav class="col-md-3">
+            <ol>
+               <?php
+                    while($row = mysqli_fetch_assoc($result)) {
+                        echo '<li><a href="http://localhost:31337/webapplication/php/data/index.php?id='.htmlspecialchars($row['id']).'">'.$row['title'].'</a></li>'."\n";
+                    }
+                ?>
+            </ol>
+        </nav>
+        <div class="col-md-9">
+            <div id="control">
+                <input type="button" value="white" id="white_btn" />
+                <input type="button" value="black" id="black_btn" />
+                <a href="http://localhost:31337/webapplication/php/data/write.php">Write</a>
+            </div>    
+            <article>
+                <?php
+                    if(!empty($_GET['id'])){
+                        $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
+                        $result = myquli_query($conn, $sql);
+                        $row = mysqli_fetch_assoc($result);
+                        echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+                        echo '<p>'.htmlspecialchars($row['name']).'</p>;
+                        echo strip_tags($row['description'], '<a><h1><h2><h3><ul><li><ol>');
+                    }
+                ?>
+            </article>
+       </div>
     <script src="http://localhost:31337/webapplication/php/data/script.js"></script>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
