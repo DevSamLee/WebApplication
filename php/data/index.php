3dcd19e9bd4c0b53498e39a60a84c6315dd95ca7
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-08-09 23:47:19 -0500

    Update index.php
    Add container class

--- php/data/index.php
+++ php/data/index.php
@@ -19,19 +19,20 @@
     
 </head>
 <body id="target">
-    <header class="jumbotron">
-        <h1><a href="http://localhost:31337/webapplication/php/data/index.php">JavaScript</a></h1>
-    </header>
-    <div class="row">
-        <nav class="col-md-3">
-            <ol>
-               <?php
-                    while($row = mysqli_fetch_assoc($result)) {
-                        echo '<li><a href="http://localhost:31337/webapplication/php/data/index.php?id='.htmlspecialchars($row['id']).'">'.$row['title'].'</a></li>'."\n";
-                    }
-                ?>
-            </ol>
-        </nav>
+    <div class="container">
+        <header class="jumbotron text-center">
+            <h1><a href="http://localhost:31337/webapplication/php/data/index.php">JavaScript</a></h1>
+        </header>
+        <div class="row">
+            <nav class="col-md-3">
+                <ol>
+                   <?php
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo '<li><a href="http://localhost:31337/webapplication/php/data/index.php?id='.htmlspecialchars($row['id']).'">'.$row['title'].'</a></li>'."\n";
+                        }
+                    ?>
+                </ol>
+            </nav>
         <div class="col-md-9">
             <div id="control">
                 <input type="button" value="white" id="white_btn" />
@@ -51,6 +52,7 @@
                 ?>
             </article>
        </div>
+    </div>
     <script src="http://localhost:31337/webapplication/php/data/script.js"></script>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
