f3582347ae05752911e728d12eb2fbabb0448701
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-08-10 22:43:02 -0500

    Update write.php
    Add a bootstrap design to write.php

--- php/data/write.php
+++ php/data/write.php
@@ -8,54 +8,60 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <link rel="stylesheet" type="text/css" href="http://localhost:31337/webapplication/php/data/style.css">
     <title>JavaScript</title>
+    
+    <!-- Bootstrap -->
+    <link href="http://localhost:31337/webapplication/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+    
 </head>
 <body id="target">
-    <header>
-        <h1><a href="http://localhost:31337/webapplication/php/data/index.php">JavaScript</a></h1>
-    </header>
-    <nav>
-        <ol>
-           <?php
-                while($row = mysqli_fetch_assoc($result)) {
-                    echo '<li><a href="http://localhost:31337/webapplication/php/data/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
-                }
-            ?>
-        </ol>
-    </nav>
-    <div id="control">
-        <input type="button" value="white" id="white_btn" />
-        <input type="button" value="black" id="black_btn" />
-        <a href="http://localhost:31337/webapplication/php/data/write.php">Write</a>
+    <div class="container">
+        <header class="jumbotron text-center">
+            <h1><a href="http://localhost:31337/webapplication/php/data/index.php">JavaScript</a></h1>
+        </header>
+        <div class="row">
+            <nav class="col-md-3">
+                <ol class="nav nav-pills nav-stacked">
+                   <?php
+                        while($row = mysqli_fetch_assoc($result)) {
+                            echo '<li><a href="http://localhost:31337/webapplication/php/data/index.php?id='.htmlspecialchars($row['id']).'">'.$row['title'].'</a></li>'."\n";
+                        }
+                    ?>
+                </ol>
+            </nav>
+        <div class="col-md-9">
+            <div id="control">
+                <div class="btn-group" role="group" aria-label="...">
+                    <input type="button" value="white" id="white_btn" class="btn btn-default" />
+                    <input type="button" value="black" id="black_btn" class="btn btn-default" />
+                </div>
+                <a href="http://localhost:31337/webapplication/php/data/write.php" class="btn btn-success">write</a>
+            </div>    
+            <article>
+               <form action="process.php" method="post">
+                    <p>
+                        Title: <input type="text" name="title">
+                    </p>
+                    <p>
+                        Author: <input type="text" name="author">
+                    </p>
+                    <p>
+                        Description: <textarea name="description" id="description"></textarea>
+                    </p>
+                    <input type="hidden" role="uploadcare-uploader" />
+                    <input type="submit" name="name">  
+                </form>
+            </article>            
+       </div>
     </div>
-    <article>
-        <form action="process.php" method="post">
-        <p>
-            Title: <input type="text" name="title">
-        </p>
-        <p>
-            Author: <input type="text" name="author">
-        </p>
-        <p>
-            Description: <textarea name="description" id="description"></textarea>
-        </p>
-        <input type="hidden" role="uploadcare-uploader" />
-        <input type="submit" name="name">  
-        </form>
-    </article>
     <script src="http://localhost:31337/webapplication/php/data/script.js"></script>
-    <script>
-        UPLOADCARE_PUBLIC_KEY = "";
-    </script>
-    <script charset="utf-8"
-       src="//ucarecdn.com/widget/2.9.0/uploadcare/uploadcare.full.min.js></script>
-    <script>
-    <!-- create widget -->
-    var singleWidget = uploadcare.SingleWidget('[role=uploadcare-uploader]');
-    singleWidget.onUploadComplete(function(info){
-       document.getElementById('description').value = document.getElementById('description').value + '<img src="'info.cdnUrl+'">'; 
-    });
-    </script>
-    </body>
-</html>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="http://localhost:31337/webapplication/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
+</body>
+</html>     
